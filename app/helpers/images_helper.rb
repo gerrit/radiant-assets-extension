026945b9d95b731276d2fe5d7f3a2e76e4e55759
Gerrit Kaiser <gerrit@gerritkaiser.de>
2011-02-24 12:51:10 +0000

    tweaked views

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -12,4 +12,13 @@ module ImagesHelper
   def image_link(image)
     link_to image.upload_uid, edit_admin_image_path(image)
   end
+  
+  def image_tag(*args)
+    image = args.first
+    if image && image.respond_to?(:upload)
+      super image.upload.url, :width => image.upload.width, :height => image.upload.height
+    else
+      super(*args)
+    end
+  end
 end
