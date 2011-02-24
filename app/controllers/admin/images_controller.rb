714660820fcdb90729141570e2e7969f17df9087
Gerrit Kaiser <gerrit@gerritkaiser.de>
2011-02-24 01:14:36 +0000

    barebones uploading via admin interface controller

--- app/controllers/admin/images_controller.rb
+++ app/controllers/admin/images_controller.rb
@@ -1,5 +1,12 @@
 class Admin::ImagesController < ApplicationController
   def index
     @images = Image.all
+    @new_image = Image.new
+  end
+  
+  def create
+    @image = Image.create! params[:image]
+    index
+    render :index, :status => :created
   end
 end
