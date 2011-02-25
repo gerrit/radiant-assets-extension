ff8fff87c19fc40c1dc280978456aab83de5ed32
Gerrit Kaiser <gerrit@gerritkaiser.de>
2011-02-25 21:11:02 +0000

    add timestamps and locking to asset model

---
+++ db/migrate/20110225210912_add_timestamps_and_locking_to_assets.rb
@@ -0,0 +1,19 @@
+class AddTimestampsAndLockingToAssets < ActiveRecord::Migration
+  def self.up
+    add_column :assets, :lock_version, :integer, :default => 0
+    add_column :assets, :created_at, :datetime
+    add_column :assets, :updated_at, :datetime
+    puts 'Updating current assets with dates'
+    Asset.all.each do |asset|
+      asset.created_at = Time.now
+      asset.updated_at = Time.now
+      asset.save!
+    end
+  end
+
+  def self.down
+    remove_column :assets, :lock_version
+    add_column :assets, :created_at
+    add_column :assets, :updated_at
+  end
+end
